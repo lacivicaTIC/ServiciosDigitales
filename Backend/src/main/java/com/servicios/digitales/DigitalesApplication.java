a674333f92bc44492867147e0651bef2c6ffa80c
arnol925 <31320890+arnol925@users.noreply.github.com>
2026-07-21 14:35:25 -0500

    primer commit backend

---
+++ Backend/src/main/java/com/servicios/digitales/DigitalesApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.digitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalesApplication.class, args);
+	}
+
+}
